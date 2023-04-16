f20f6407d054fed2bac151305e7efb30cb4589a4
kopolot <konradmajkrzyk@gmail.com>
2023-04-16 23:47:12 +0200

    coś łapie

---
+++ components/Test.tsx
@@ -0,0 +1,9 @@
+// import React, {useState} from "react";
+// import { StyleSheet, Text,View } from "react-native/types";
+
+// export type Test = {
+//     name: string
+//     age: number
+// }
+
+// const Test
